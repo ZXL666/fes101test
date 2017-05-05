0ddd02aa6a1c5ac452735a1f42b95597f9787286
朱小丽 <122627275@qq.com>
2017-05-05 17:19:12 +0800

    first commit

---
+++ fes101test/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace fes101test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP：";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(64, 17);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(100, 21);
+            this.textBoxIP.TabIndex = 1;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxPort);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxIP);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(590, 471);
+            this.splitContainer1.SplitterDistance = 62;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(322, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(232, 17);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(51, 21);
+            this.textBoxPort.TabIndex = 1;
+            this.textBoxPort.Text = "10500";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port：";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer2.Panel1.Controls.Add(this.toolStrip2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer2.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer2.Size = new System.Drawing.Size(590, 405);
+            this.splitContainer2.SplitterDistance = 177;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("微软雅黑", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 25);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(588, 150);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(588, 25);
+            this.toolStrip2.TabIndex = 3;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel1.Text = "发送区";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("微软雅黑", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(0, 25);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(588, 197);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(588, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel2.Text = "接收区";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 471);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "fes101test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+    }
+}
+
---
+++ fes101test/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+
+namespace fes101test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Socket conn = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static Thread NetSendThread, NetRecvThread;
+        private delegate void LogAppendDelegate(Color color, string text);
+        int index = 0;
+        List<string> TxList = new List<string>();
+        byte[]Sendadr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "打开")
+            {
+                conn = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                conn.Connect(IPAddress.Parse(textBoxIP.Text.Trim()), Convert.ToInt16(textBoxPort.Text.Trim()));
+
+                NetSendThread = new Thread(new ThreadStart(SendProc));
+                NetSendThread.Start();
+
+                NetRecvThread = new Thread(new ThreadStart(ReceiveProc));
+                NetRecvThread.Start();
+                button1.Text = "关闭";
+            }
+            else
+            {
+                conn.Close();
+                if (NetSendThread != null)
+                    NetSendThread.Abort();
+                if (NetRecvThread != null)
+                    NetRecvThread.Abort();
+                button1.Text = "打开";
+            }
+        }
+        private void LogAppend(Color color, string text)
+        {
+            if (richTextBox1.ReadOnly == true)
+                return;
+            richTextBox1.SelectionColor = color;
+            richTextBox1.AppendText(text);
+            richTextBox1.ScrollToCaret();
+        }
+        private void SendMessage(string text)
+        {
+            LogAppendDelegate la = new LogAppendDelegate(LogAppend);
+            richTextBox1.Invoke(la, Color.Black, text);
+        }
+        private void ReceiveMessage(string text)
+        {
+            LogAppendDelegate la = new LogAppendDelegate(LogAppend);
+            richTextBox2.Invoke(la, Color.Green, text);
+        }
+
+        private void  StrToByteArray(string str)
+        {
+            str = str.Replace(" ","");
+            int len = (str.Length) / 2;
+            Sendadr = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                Sendadr[i] = Convert.ToByte(str.Substring(i * 2, 2), 16);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string FileName = System.Environment.CurrentDirectory + "\\Cfg.txt";
+            FileStream afile;
+            StreamReader sr;
+            string strline="";
+            afile = new FileStream(FileName, FileMode.Open);//
+            sr = new StreamReader(afile);
+
+            while (strline != "Start")
+            {
+                strline = sr.ReadLine();
+            }
+            while (true)
+            {
+                strline = sr.ReadLine();
+                if (strline.IndexOf("End") >= 0)
+                    break;
+                TxList.Add(strline);
+               
+            }
+            sr.Close();
+            afile.Close();
+        }
+        private void SendProc()   //网络发送数据线程
+        {
+            while (true)    //处理事物
+            {
+                if (index >= TxList.Count)
+                {
+                    index = 0;
+                    return;
+                }
+                try
+                {
+                    StrToByteArray(TxList[index]);
+                    conn.Send(Sendadr, 0, Sendadr.Length, 0);
+                    StringBuilder str = new StringBuilder("发送:");
+                    for (int i = 0; i < Sendadr.Length; i++)
+                    {
+                        Invoke(new Action(() =>
+                        {
+                            str.Append(" ");
+                            str.Append(Sendadr[i].ToString("X2"));
+                        }));
+                    }
+                    SendMessage(str + string.Format(" ({0})", System.DateTime.Now) + "\n");
+                    index++;
+                }
+                catch
+                {
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void ReceiveProc()
+        {
+            int NetRLen=0;
+            byte[] NetRBuffer = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                     NetRLen = conn.Receive(NetRBuffer);
+                    if (NetRLen > 0)
+                    {
+                        StringBuilder str = new StringBuilder("接收:");
+                        for (int i = 0; i < NetRLen; i++)
+                        {
+                            Invoke(new Action(() =>
+                            {
+                                str.Append(" ");
+                                str.Append(NetRBuffer[i].ToString("X2"));
+                            }));
+                        }
+                        ReceiveMessage(str + string.Format(" ({0})", System.DateTime.Now) + "\n");
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch
+                {
+                }
+                Thread.Sleep(1);
+
+            }
+        }
+    }
+}
+
